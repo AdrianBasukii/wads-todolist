158458148c86083901d41fefa9c98cedced4fc70
Adrian <adriannugrohobasuki@gmail.com>
2025-03-13 14:13:15 +0700

    Fixed image directory

--- src/components/imagecontainer.jsx
+++ src/components/imagecontainer.jsx
@@ -1,4 +1,5 @@
 import Circles from "./circles"
+import landingImage from "../assets/landing_image.png";
 
 export default function ImageContainer(){
     return(
@@ -8,7 +9,7 @@ export default function ImageContainer(){
                 <Circles />
                 <h1 className="text-neutral-400 text-sm">To-Do App Image</h1>
             </div>
-            <img className="w-full rounded-bl-xl rounded-br-xl border-b-2 border-r-2 border-l-2 border-neutral-900" src="src/assets/landing_image.png" alt="to-do app image" />
+            <img className="w-full rounded-bl-xl rounded-br-xl border-b-2 border-r-2 border-l-2 border-neutral-900" src={landingImage} alt="to-do app image" />
         </div>
     )
 }
